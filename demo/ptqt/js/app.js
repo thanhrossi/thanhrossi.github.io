3e2ef296f437c9405515e2dd207a06e83421d25d
thanh <thanhrossi.tt@gmail.com>
2015-09-11 14:34:38 +0700

    add preload

--- demo/ptqt/js/app.js
+++ demo/ptqt/js/app.js
@@ -1,6 +1,12 @@
 $(document).foundation();
+// preload
+$(window).load(function() {
+    $('.preload').fadeOut();
+
+});
 // TinyNav.js 1
 $('ul#menu-topmenu').tinyNav({
     active: 'selected',
     header: '-- Menu --',
 });
+
